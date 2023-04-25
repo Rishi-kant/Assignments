95df30280cdec9da8e5fe1feca16c6c47456d7de
Rishi-Kant <kant3397@gmail.com>
2023-04-25 09:37:49 +0530

    Todo using Redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addTodo, removeTodo,toggleComplete } from './Features/Todo/TodoSlice';
+import style from './App.module.css';
+
 
 function App() {
+  const [text, setText] = useState('');
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    dispatch(addTodo(text));
+    setText('');
+  };
+
+  const handleRemove = (id) => {
+    dispatch(removeTodo(id));
+  };
+  const handleToggleComplete = (id) => {
+    dispatch(toggleComplete(id));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={style.container}>
+      <div className={style.name}>
+        <h2>Todo Application</h2>
+      </div>
+      <form onSubmit={handleSubmit}>
+        <input value={text} onChange={(e) => setText(e.target.value)} />
+        <button className={style.btn}>Add Todo</button>
+      </form>
+      <ul>
+        {todos.map((todo) => (
+          <>
+          <li className={style.content}>
+            <p key={todo.id} style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}>
+              {todo.text}
+            </p>
+           <div>
+            <button onClick={()=>handleToggleComplete(todo.id)} className={style.addBtn}>
+              completed
+            </button>
+            </div>
+            <div>
+            <button onClick={() => handleRemove(todo.id)} className={style.addBtn}>Remove</button>
+            </div>
+            </li>
+
+          </>
+        ))}
+      </ul>
     </div>
   );
 }
