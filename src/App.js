d22e61419df0ec9b85d6b79b31c1df1779c0a3bd
Rishi-Kant <kant3397@gmail.com>
2023-05-04 09:30:12 +0530

    serchable Drop Down

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import * as React from "react";
+import Autocomplete from "@mui/material/Autocomplete";
+import TextField from "@mui/material/TextField";
+import Stack from "@mui/material/Stack";
+import country from "./countries.json";
 
-function App() {
+export default function Tags() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Stack
+      spacing={3}
+      sx={{
+        display: "flex",
+        height: "100vh",
+        justifyContent: "center",
+      }}
+    >
+      <Autocomplete
+        sx={{ width: "40%", alignSelf: "center" }}
+        multiple
+        id="tags-outlined"
+        options={country}
+        getOptionLabel={(option) => option.country}
+        defaultValue={[country[13]]}
+        filterSelectedOptions
+        renderInput={(params) => (
+          <TextField {...params} label="Country" placeholder="Favorites" />
+        )}
+      />
+    </Stack>
   );
 }
 
-export default App;
+// Top 100 films as rated by IMDb users. http://www.imdb.com/chart/top
